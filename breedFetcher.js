bb224f162ed8b6b81d052199d20f72281b44a894
oceanway1 <oceanway1@yahoo.ca>
2019-12-06 01:17:31 +0000

    final

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,23 @@
-const input = process.argv.slice(2);
 const request = require('request');
-const data = JSON.parse(body);
-request('https://api.thecatapi.com/v1/breeds/search?q=' + input, (error, response, body) => {
-  if (error) {
-    console.log('error, breed', error);
-  }
-  else if (response) {
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  }
-  let result = "";
-  // console.log(data);
-  console.log(data[0].description)
-  // console.log(data['descripton']);
-  // console.log(data.description[0]);
-  //   for ( let item of data) {
-  //  result = data[0];
-  // return result;
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, body) => {
+    const data = JSON.parse(body);
+    let result = data[0].description;
+    callback(error, result);
+    // if (error === null) {
+    //   console.log('error, breed', error);
+    // }
+    //else if (response) {
+    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // }
+    // // let result = "";
+    // // console.log(data);
+    // console.log(data[0].description)
+    // console.log(data['descripton']);
+    // console.log(data.description[0]);
+  });
+};
+module.exports = {
+  fetchBreedDescription
+};
